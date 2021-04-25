ae3ed10d914ed3f710ffb2708cebcee6a3aa62b4
Nilesh Indore <indorenilesh@gmail.com>
2021-04-25 09:36:08 +0530

    update

---
+++ scripts/userdata.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.org
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+service sshd restart
+pass=$(perl -e 'print crypt($ARGV[0], "password")' admin)
+useradd -m -p "$pass" "admin"
