5db66e99cf5d911e97ffbf6a147f1a57a37cc461
Nilesh Indore <indorenilesh@gmail.com>
2021-04-25 09:52:03 +0530

    update

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -4,3 +4,4 @@ sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_
 service sshd restart
 pass=$(perl -e 'print crypt($ARGV[0], "password")' admin)
 useradd -m -p "$pass" "admin"
+echo "admin    ALL=(ALL)       NOPASSWD:ALL" >> /etc/sudoers
